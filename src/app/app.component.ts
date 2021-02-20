0f3a3b86b68d49885bb893c7f89cb998a2714941
SabrinaFiore <sabrinafiore1992@gmail.com>
2021-02-20 08:54:11 +0100

    index: get data from json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 's-json-call';
+
+  url = 'https://jsonplaceholder.typicode.com/todos?fbclid=IwAR2wcbmzd8WCrAoh3XhMUcAmEE5XnvTdzs_lbKx7dt5MCpnx9f2Din7q-3s';
+  items = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).toPromise().then(data => {
+      console.log(data);
+
+      for (let key in data) if (data.hasOwnProperty(key))
+        this.items.push(data[key]);
+    });
+  }
 }
